b856a4cf4de4c1170a1e22e6e9e7140e19539972
David Littlewood <djlittl@sandia.gov>
2018-10-24 14:12:10 -0600

    Added LaTex output.

--- gear_list.py
+++ gear_list.py
@@ -40,3 +40,38 @@ if __name__ == "__main__":
     base_weight *= 0.00220462
 
     print "\nBase weight:", base_weight, "lb.\n"
+
+    tex =  "\documentclass[letterpaper, 12pt]{article}\n"
+    tex += "\usepackage{fullpage}\n"
+    tex += "\\title{Backpacking Gear List}\n"
+    tex += "\\author{}\n"
+    tex += "\date{}\n"
+    tex += "\\begin{document}\n"
+    tex += "\maketitle\n"
+    tex += "\\thispagestyle{empty}\n"
+
+    for key in gear.keys():
+        item = gear[key]
+        num_base = 0
+        if 'base' in item.keys():
+            num_base = int(item['base'])
+        num_nonbase = 0
+        if 'nonbase' in item.keys():
+            num_nonbase = int(item['nonbase'])
+        num_worn = 0
+        if 'num_worn' in item.keys():
+            num_worn = int(item['worn'])
+        num_total = num_base + num_nonbase + num_worn
+        if num_total > 0:
+            entry = "\n" + item['type'] + ": " + key
+            if num_total > 1:
+                entry += " (x" + str(num_total) + ")"
+            entry += "\n"
+            tex += entry
+
+    tex += "\end{document}\n"
+
+    tex_file = open("gear_list.tex", 'w')
+    tex_file.write(tex)
+    tex_file.close()
+    print "LaTex written to gear_list.tex\n"
